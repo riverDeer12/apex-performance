615bab81e53928668a6a3d41e926a4701f5917a6
riverDeer12 <mtrbojevic12@gmail.com>
2025-04-28 00:35:35 +0200

    testing sending email for registration confirmation

--- Constants/ErrorMessages.cs
+++ Constants/ErrorMessages.cs
@@ -10,4 +10,5 @@ public static class ErrorMessages
     public const string SavingError = nameof(SavingError);
     public const string UnauthorizedAction = nameof(UnauthorizedAction);
     public const string AlreadyExists = nameof(UnauthorizedAction);
+    public const string ErrorSendingEmail = nameof(ErrorSendingEmail);
 }
