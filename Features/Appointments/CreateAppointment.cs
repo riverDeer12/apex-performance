58d6e39ab57ff4558f80b671819c55c10d61d138
riverDeer12 <mtrbojevic12@gmail.com>
2025-05-06 15:34:02 +0200

    added logic for creating and updating appointment

--- Features/Appointments/CreateAppointment.cs
+++ Features/Appointments/CreateAppointment.cs
@@ -1,13 +1,33 @@
 using ApexPerformance.API.Constants;
 using ApexPerformance.API.Database;
+using ApexPerformance.API.Database.Entities;
 using FastEndpoints;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApexPerformance.API.Features.Appointments;
 
-public record CreateAppointmentRequest();
-public record CreateAppointmentResponse();
+public record CreateAppointmentRequest(
+    Guid AppointmentType,
+    DateTimeOffset StartTime,
+    DateTimeOffset EndTime,
+    List<Guid> Clients
+);
 
-public class CreateAppointmentEndpoint: Endpoint<CreateAppointmentRequest, CreateAppointmentResponse>
+public record CreateAppointmentResponse(
+    Guid AppointmentId,
+    DateTimeOffset StartTime,
+    DateTimeOffset EndTime,
+    List<AppointmentClientDto> Clients
+);
+
+public record AppointmentClientDto(
+    Guid Id,
+    string Firstname,
+    string Lastname
+);
+
+public class CreateAppointmentEndpoint : Endpoint<CreateAppointmentRequest, CreateAppointmentResponse>
 {
     private readonly ApexPerformanceContext _context;
 
@@ -15,7 +35,7 @@ public class CreateAppointmentEndpoint: Endpoint<CreateAppointmentRequest, Creat
     {
         _context = context;
     }
-    
+
     public override void Configure()
     {
         Post("api/appointments");
@@ -23,8 +43,70 @@ public class CreateAppointmentEndpoint: Endpoint<CreateAppointmentRequest, Creat
         Options(x => x.WithTags("Appointments"));
     }
 
-    public override async Task HandleAsync(CreateAppointmentRequest request, CancellationToken ct)
+    public override async Task HandleAsync(CreateAppointmentRequest request, CancellationToken cancellationToken)
     {
-        await base.HandleAsync(request, ct);
+        var clients = await _context.Clients
+            .Where(x => request.Clients.Contains(x.Id))
+            .ToListAsync(cancellationToken: cancellationToken);
+
+        if (clients.Count == 0)
+            ThrowError(ErrorMessages.NotFound);
+
+        var appointmentType = await _context.AppointmentTypes
+            .FirstOrDefaultAsync(x => x.Id == request.AppointmentType,
+                cancellationToken: cancellationToken);
+
+        if (appointmentType is null)
+            ThrowError(ErrorMessages.NotFound);
+
+        var appointment = new Appointment
+        {
+            StartTime = request.StartTime,
+            EndTime = request.EndTime,
+            AppointmentType = appointmentType
+        };
+
+        _context.Appointments.Add(appointment);
+
+        var result = await _context.SaveChangesAsync(cancellationToken);
+
+        if (result == 0)
+            ThrowError(ErrorMessages.SavingError);
+
+        var clientAppointments = clients
+            .Select(client => new ClientAppointment
+            {
+                ClientId = client.Id,
+                Client = client,
+                AppointmentId = appointment.Id,
+                Appointment = appointment
+            }).ToList();
+
+        _context.ClientAppointments.AddRange(clientAppointments);
+
+        var relationsResult = await _context.SaveChangesAsync(cancellationToken);
+
+        if (relationsResult == 0)
+            ThrowError(ErrorMessages.SavingError);
+
+        var clientsResponse = clients
+            .Select(client => new AppointmentClientDto(client.Id, client.FirstName, client.LastName))
+            .ToList();
+
+        await SendAsync(
+            new CreateAppointmentResponse(appointment.Id, appointment.StartTime, appointment.EndTime,
+                clientsResponse),
+            cancellation: cancellationToken);
+    }
+}
+
+public sealed class CreateAppointmentValidator : Validator<CreateAppointmentRequest>
+{
+    public CreateAppointmentValidator()
+    {
+        RuleFor(x => x.AppointmentType).NotEmpty().WithMessage(ValidationMessages.Required);
+        RuleFor(x => x.StartTime).NotEmpty().WithMessage(ValidationMessages.Required);
+        RuleFor(x => x.EndTime).NotEmpty().WithMessage(ValidationMessages.Required);
+        RuleFor(x => x.Clients).NotEmpty().WithMessage(ValidationMessages.Required);
     }
 }
--- Features/Appointments/UpdateAppointment.cs
+++ Features/Appointments/UpdateAppointment.cs
@@ -1,11 +1,25 @@
 using ApexPerformance.API.Constants;
 using ApexPerformance.API.Database;
+using ApexPerformance.API.Database.Entities;
 using FastEndpoints;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApexPerformance.API.Features.Appointments;
 
-public record UpdateAppointmentRequest();
-public record UpdateAppointmentResponse();
+public record UpdateAppointmentRequest(
+    Guid AppointmentType,
+    DateTimeOffset StartTime,
+    DateTimeOffset EndTime,
+    List<Guid> Clients
+);
+
+public record UpdateAppointmentResponse(
+    Guid AppointmentId,
+    DateTimeOffset StartTime,
+    DateTimeOffset EndTime,
+    List<AppointmentClientDto> Clients
+);
 
 public class UpdateAppointmentEndpoint : Endpoint<UpdateAppointmentRequest, UpdateAppointmentResponse>
 {
@@ -15,7 +29,7 @@ public class UpdateAppointmentEndpoint : Endpoint<UpdateAppointmentRequest, Upda
     {
         _context = context;
     }
-    
+
     public override void Configure()
     {
         Put("api/appointments/{id}");
@@ -23,8 +37,59 @@ public class UpdateAppointmentEndpoint : Endpoint<UpdateAppointmentRequest, Upda
         Options(x => x.WithTags("Appointments"));
     }
 
-    public override async Task HandleAsync(UpdateAppointmentRequest request, CancellationToken ct)
+    public override async Task HandleAsync(UpdateAppointmentRequest request, CancellationToken cancellationToken)
     {
-        await base.HandleAsync(request, ct);
+        var appointmentId = Route<Guid>("id", isRequired: true);
+
+        var appointment =
+            await _context.Appointments
+                .FirstOrDefaultAsync(x => x.Id == appointmentId, cancellationToken: cancellationToken);
+
+        if (appointment is null)
+            ThrowError(ErrorMessages.NotFound);
+
+        appointment.StartTime = request.StartTime;
+        appointment.EndTime = request.EndTime;
+
+        appointment.Clients = new List<ClientAppointment>(
+            request.Clients.Select(clientId => new ClientAppointment
+            {
+                ClientId = clientId,
+                AppointmentId = appointment.Id
+            }));
+
+        await _context.ClientAppointments
+            .Where(clientAppointment => clientAppointment.AppointmentId == appointment.Id)
+            .ExecuteDeleteAsync(cancellationToken);
+
+        _context.Appointments.Update(appointment);
+
+        var result = await _context.SaveChangesAsync(cancellationToken: cancellationToken);
+
+        if (result == 0)
+            ThrowError(ErrorMessages.SavingError);
+
+        var clients = await _context.Clients
+            .Where(x => request.Clients.Contains(x.Id))
+            .ToListAsync(cancellationToken: cancellationToken);
+
+        var clientsResponse = clients
+            .Select(client => new AppointmentClientDto(client.Id, client.FirstName, client.LastName))
+            .ToList();
+
+        await SendAsync(
+            new UpdateAppointmentResponse(appointment.Id, appointment.StartTime, appointment.EndTime, clientsResponse),
+            cancellation: cancellationToken);
+    }
+}
+
+public sealed class UpdateAppointmentValidator : Validator<UpdateAppointmentRequest>
+{
+    public UpdateAppointmentValidator()
+    {
+        RuleFor(x => x.AppointmentType).NotEmpty().WithMessage(ValidationMessages.Required);
+        RuleFor(x => x.StartTime).NotEmpty().WithMessage(ValidationMessages.Required);
+        RuleFor(x => x.EndTime).NotEmpty().WithMessage(ValidationMessages.Required);
+        RuleFor(x => x.Clients).NotEmpty().WithMessage(ValidationMessages.Required);
     }
 }
