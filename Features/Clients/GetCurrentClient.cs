d0bb87c8038d0dc37d2c972a7031c7aae00201ef
riverDeer12 <mtrbojevic12@gmail.com>
2025-06-05 23:14:54 +0200

    added endpoint for getting data for currently logged in client

---
+++ Features/Clients/GetCurrentClient.cs
@@ -0,0 +1,62 @@
+﻿using ApexPerformance.API.Constants;
+using ApexPerformance.API.Database;
+using ApexPerformance.API.Services;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApexPerformance.API.Features.Clients;
+
+public record GetCurrentClientResponse(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Phone,
+    int Credits,
+    ClientUserDto User
+);
+
+public class GetCurrentClientEndpoint : EndpointWithoutRequest<GetCurrentClientResponse>
+{
+    private readonly ApexPerformanceContext _context;
+    private readonly ICurrentUserService _currentUserService;
+
+    public GetCurrentClientEndpoint(ApexPerformanceContext context, ICurrentUserService currentUserService)
+    {
+        _context = context;
+        _currentUserService = currentUserService;
+    }
+
+    public override void Configure()
+    {
+        Get("api/clients/current-client");
+        Options(x => x.WithTags("Clients"));
+    }
+
+    public override async Task HandleAsync(CancellationToken cancellationToken)
+    {
+        var client = await _context.Clients.FirstOrDefaultAsync(x => x.UserId == _currentUserService.UserId,
+            cancellationToken: cancellationToken);
+
+        if (client is null)
+            ThrowError(ErrorMessages.NotFound);
+
+        var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == _currentUserService.UserId,
+            cancellationToken: cancellationToken);
+        
+        if (user is null)
+            ThrowError(ErrorMessages.NotFound);
+
+        var userResponse = new ClientUserDto(user.Id, user.UserName, user.Email);
+
+        await SendAsync(new GetCurrentClientResponse(
+            client.Id,
+            client.FirstName,
+            client.LastName,
+            client.Email,
+            client.Phone,
+            client.Credits,
+            userResponse
+            ), cancellation: cancellationToken);
+    }
+}
