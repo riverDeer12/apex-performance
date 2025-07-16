587c2a3ef305cd18f9322591f47fde3fd3539c3c
riverDeer12 <mtrbojevic12@gmail.com>
2025-07-16 09:04:22 +0200

    created endpoints for coach time slots

---
+++ Features/Coaches/TimeSlots/GetAllTimeSlots.cs
@@ -0,0 +1,38 @@
+using ApexPerformance.API.Constants;
+using ApexPerformance.API.Database;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApexPerformance.API.Features.Coaches.TimeSlots;
+
+public record GetTimeSlotResponse(Guid Id, DayOfWeek Day, TimeOnly StartTime, TimeOnly EndTime);
+
+public class GetAllTimeSlotsEndpoint : EndpointWithoutRequest<List<GetTimeSlotResponse>>
+{
+    private readonly ApexPerformanceContext _context;
+
+    public GetAllTimeSlotsEndpoint(ApexPerformanceContext context)
+    {
+        _context = context;
+    }
+
+    public override void Configure()
+    {
+        Get("api/coaches/time-slots/all");
+        Roles(nameof(UserRoles.SuperAdmin), nameof(UserRoles.Administrator));
+    }
+
+    public override async Task HandleAsync(CancellationToken cancellationToken)
+    {
+        var timeSlots = await _context.TimeSlots.ToListAsync(cancellationToken: cancellationToken);
+
+        if (timeSlots.Count == 0)
+        {
+            await SendAsync([], cancellation: cancellationToken);
+            return;
+        }
+
+        await SendAsync(timeSlots.Select(x
+            => new GetTimeSlotResponse(x.Id, x.Day, x.StartTime, x.EndTime)).ToList(), cancellation: cancellationToken);
+    }
+}
---
+++ Features/Coaches/TimeSlots/GetCoachTimeSlots.cs
@@ -0,0 +1,51 @@
+using ApexPerformance.API.Constants;
+using ApexPerformance.API.Database;
+using ApexPerformance.API.Services;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApexPerformance.API.Features.Coaches.TimeSlots;
+
+public class GetCoachTimeSlotsEndpoint : EndpointWithoutRequest<List<GetTimeSlotResponse>>
+{
+    private readonly ApexPerformanceContext _context;
+    private readonly ICurrentUserService _currentUserService;
+
+    public GetCoachTimeSlotsEndpoint(ApexPerformanceContext context, ICurrentUserService currentUserService)
+    {
+        _context = context;
+        _currentUserService = currentUserService;
+    }
+
+    public override void Configure()
+    {
+        Get("api/coaches/time-slots/{id}");
+    }
+
+    public override async Task HandleAsync(CancellationToken cancellationToken)
+    {
+        var coachId = Route<Guid>("id", isRequired: true);
+
+        var coach = await _context.Coaches.FirstOrDefaultAsync(x => x.Id == coachId,
+            cancellationToken: cancellationToken);
+
+        if (coach is null)
+            ThrowError(ErrorMessages.NotFound);
+
+        var timeSlots = await _context.CoachTimeSlots
+            .Where(x => x.CoachId == coachId)
+            .Select(x => x.TimeSlotId)
+            .ToListAsync(cancellationToken: cancellationToken);
+
+        if (timeSlots.Count == 0)
+        {
+            await SendAsync([], cancellation: cancellationToken);
+            return;
+        }
+
+        var coachTimeSlots = _context.TimeSlots.Where(x => timeSlots.Contains(x.Id)).ToList();
+
+        await SendAsync(coachTimeSlots.Select(x
+            => new GetTimeSlotResponse(x.Id, x.Day, x.StartTime, x.EndTime)).ToList(), cancellation: cancellationToken);
+    }
+}
