f974b0b76d0d609b43f06b389821b1110b53c92a
riverDeer12 <mtrbojevic12@gmail.com>
2025-08-01 16:33:10 +0200

    added client and coach role assignment or registration updated validation messages updated reset password

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -14,13 +14,27 @@ public class UserService : IUserService
         _context = context;
     }
 
-    public async Task<User> CreateUserAccount(string username, string password, string email,
+    public async Task<User> CreateUserAccount(string username, string password, string email, Role? role,
         CancellationToken cancellationToken)
     {
         if (await UsernameExists(username, cancellationToken))
             throw new Exception(ValidationMessages.UsernameAlreadyExists);
-        
+
+        if (await EmailExists(username, cancellationToken))
+            throw new Exception(ValidationMessages.EmailAlreadyExists);
+
         var user = User.Init(username, password, email);
+
+        if (role is not null)
+        {
+            var userRole = new UserRole
+            {
+                UserId = user.Id,
+                RoleId = role.Id
+            };
+
+            _context.UserRoles.Add(userRole);
+        }
 
         _context.Users.Add(user);
 
@@ -30,6 +44,12 @@ public class UserService : IUserService
             throw new Exception(ErrorMessages.SavingError);
 
         return user;
+    }
+
+    private async Task<bool> EmailExists(string email, CancellationToken cancellationToken)
+    {
+        return await _context.Users
+            .AnyAsync(x => x.Email == email, cancellationToken);
     }
 
     public async Task<bool> UsernameExists(string username, CancellationToken cancellationToken)
