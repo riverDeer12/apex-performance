befca9d7473d8500606ecddd56b83eb50f921e2f
mtrbojevic12 <mtrbojevic12@gmail.com>
2025-08-08 16:31:38 +0200

    current coach endpoint

---
+++ Features/Coaches/GetCurrentCoach.cs
@@ -0,0 +1,41 @@
+using ApexPerformance.API.Constants;
+using ApexPerformance.API.Database;
+using ApexPerformance.API.Services;
+using ApexPerformance.API.Shared.DataTransferObjects;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApexPerformance.API.Features.Coaches;
+
+public class GetCurrentCoachEndpoint: EndpointWithoutRequest<PersonDataDto>
+{
+    private readonly ApexPerformanceContext _context;
+    private readonly ICurrentUserService _currentUserService;
+
+    public GetCurrentCoachEndpoint(ApexPerformanceContext context, ICurrentUserService currentUserService)
+    {
+        _context = context;
+        _currentUserService = currentUserService;
+    }
+
+    public override void Configure()
+    {
+        Get("api/coaches/current-coach");
+        Options(x => x.WithTags("Coaches"));
+    }
+
+    public override async Task HandleAsync(CancellationToken cancellationToken)
+    {
+        var coach = await _context.Clients.FirstOrDefaultAsync(x => x.UserId == _currentUserService.UserId,
+            cancellationToken: cancellationToken);
+
+        if (coach is null)
+            ThrowError(ErrorMessages.NotFound);
+        
+        await SendAsync(new PersonDataDto(
+            coach.Id,
+            coach.FirstName,
+            coach.LastName
+        ), cancellation: cancellationToken);
+    }
+}
