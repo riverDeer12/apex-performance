af81121e3adb7b458cfe6ed08fe536b8a611c2f3
mtrbojevic12 <mtrbojevic12@gmail.com>
2025-08-22 11:04:31 +0200

    finished endpoint for available recurring time slots for coach

--- Features/TimeSlots/GetRecurringAvailableTimeSlots.cs
+++ Features/TimeSlots/GetRecurringAvailableTimeSlots.cs
@@ -1,6 +1,59 @@
+using ApexPerformance.API.Constants;
+using ApexPerformance.API.Database;
+using ApexPerformance.API.Shared.DataTransferObjects;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
 namespace ApexPerformance.API.Features.TimeSlots;
 
-public class GetRecurringAvailableTimeSlots
+public class GetRecurringAvailableTimeSlotsEndpoint : EndpointWithoutRequest<List<GetTimeSlotResponse>>
 {
-    
+    private readonly ApexPerformanceContext _context;
+
+    public GetRecurringAvailableTimeSlotsEndpoint(ApexPerformanceContext context)
+    {
+        _context = context;
+    }
+
+    public override void Configure()
+    {
+        Get("api/recurring-appointments/available/{id}");
+        Options(x => x.WithTags("RecurringAppointments"));
+    }
+
+    public override async Task HandleAsync(CancellationToken cancellationToken)
+    {
+        var coachId = Route<Guid>("id", isRequired: true);
+
+        var coach = await _context.Coaches.FirstOrDefaultAsync(x => x.Id == coachId,
+            cancellationToken: cancellationToken);
+
+        if (coach is null)
+            ThrowError(ErrorMessages.NotFound);
+
+        var coachTimeSlots = await _context.CoachTimeSlots
+            .Where(x => x.CoachId == coach.Id)
+            .Include(coachTimeSlot => coachTimeSlot.TimeSlot)
+            .ToListAsync(cancellationToken: cancellationToken);
+
+        if (coachTimeSlots.Count == 0)
+        {
+            await SendAsync([], cancellation: cancellationToken);
+            return;
+        }
+
+        var takenTimeSlotIds = await _context.RecurringAppointments.Where(x => x.CoachId == coach.Id)
+            .Select(x => x.TimeSlotId)
+            .ToListAsync(cancellationToken: cancellationToken);
+
+        var availableTimeSlots = coachTimeSlots
+            .Where(x => !takenTimeSlotIds.Contains(x.TimeSlotId))
+            .ToList();
+
+        await SendAsync(availableTimeSlots.Select(x =>
+            new GetTimeSlotResponse(x.TimeSlot.Id, x.TimeSlot.Name,
+                Enum.GetName(typeof(DayOfWeek), x.TimeSlot.Day)!,
+                x.TimeSlot.StartTime,
+                x.TimeSlot.EndTime)).ToList(), cancellation: cancellationToken);
+    }
 }
