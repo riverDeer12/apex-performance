5ff4e8597414aa276fcf38c1b94e093f9ac70c78
Milan Trbojevic <mtrbojevic12@gmail.com>
2026-02-06 23:25:36 +0100

    created migration for m-m change on workouts and workout types

---
+++ Database/Migrations/20260206220531_Added workouts and workout types m-m.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApexPerformance.API.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedworkoutsandworkouttypesmm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Workouts_WorkoutTypes_WorkoutTypeId",
+                table: "Workouts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Workouts_WorkoutTypeId",
+                table: "Workouts");
+
+            migrationBuilder.DropColumn(
+                name: "WorkoutTypeId",
+                table: "Workouts")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "WorkoutsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.CreateTable(
+                name: "WorkoutWorkoutTypes",
+                columns: table => new
+                {
+                    WorkoutId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WorkoutTypeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkoutWorkoutTypes", x => new { x.WorkoutId, x.WorkoutTypeId });
+                    table.ForeignKey(
+                        name: "FK_WorkoutWorkoutTypes_WorkoutTypes_WorkoutTypeId",
+                        column: x => x.WorkoutTypeId,
+                        principalTable: "WorkoutTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_WorkoutWorkoutTypes_Workouts_WorkoutId",
+                        column: x => x.WorkoutId,
+                        principalTable: "Workouts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkoutWorkoutTypes_WorkoutTypeId",
+                table: "WorkoutWorkoutTypes",
+                column: "WorkoutTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkoutWorkoutTypes");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "WorkoutTypeId",
+                table: "Workouts",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"))
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "WorkoutsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workouts_WorkoutTypeId",
+                table: "Workouts",
+                column: "WorkoutTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Workouts_WorkoutTypes_WorkoutTypeId",
+                table: "Workouts",
+                column: "WorkoutTypeId",
+                principalTable: "WorkoutTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
