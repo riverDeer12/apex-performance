c78f8d4aff807b8263ca0db4e03c78db56c4fab0
Milan Trbojevic <mtrbojevic12@gmail.com>
2026-02-16 16:19:36 +0100

    removed stripe secret

--- Features/Clients/GetClients.cs
+++ Features/Clients/GetClients.cs
@@ -1,8 +1,6 @@
 using ApexPerformance.API.Constants;
 using ApexPerformance.API.Database;
 using ApexPerformance.API.Database.Entities;
-using ApexPerformance.API.Features.BodyMeasurements;
-using ApexPerformance.API.Services;
 using ApexPerformance.API.Services.Interfaces;
 using ApexPerformance.API.Shared.DataTransferObjects;
 using FastEndpoints;
